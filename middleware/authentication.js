e9192cb01402a08ec00a3f518b49f095181ce863
Mudassir5367 <mudassirhussain32303@gmail.com>
2025-03-11 23:50:28 +0500

    authentication

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -5,7 +5,7 @@ const authentication = async (req, res, next) => {
     const authHeader = req.headers.authorization;
     if (!authHeader) return res.status(401).json({ message: 'Access Denied' });
 
-    const token = authHeader.split(' ')[1]; 
+    const token = authHeader.split(' ')[1];  
     console.log('token', token);
     
 
