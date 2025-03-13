8497d9609945bb3a415903e9ceb0f9229a48f536
Mudassir5367 <mudassirhussain32303@gmail.com>
2025-03-13 21:57:51 +0500

    routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 require('../db/db')
+const authentication = require('../middleware/authentication')
 
 const rejister = require('../controllers/register' ) 
 const login = require('../controllers/login' ) 
@@ -10,6 +11,6 @@ const getAllPosts = require('../controllers/getAllPosts' )
 router.post('/api/register', rejister)
 router.post('/api/login', login)
 router.post('/api/allPosts', allPosts)
-router.get('/api/getAllPosts', getAllPosts)
+router.get('/api/getAllPosts', authentication, getAllPosts)
 
 module.exports = router
