8fdaf6f814f4855b3072a96a1b2555b48437b2f9
Mudassir5367 <mudassirhussain32303@gmail.com>
2025-03-19 23:06:41 +0500

    .

--- controllers/getAllPosts.js
+++ controllers/getAllPosts.js
@@ -4,7 +4,6 @@ const getAllPosts = async (req, res) => {
     try {
         const userId = req.user._id;
         const data = await AllPosts.find({ userId: req.user._id }).sort({ createdAt: -1 });
-        
         return res.status(200).json({ success: true, data });
     } catch (error) {
         return res.status(500).json({ success: false, message: 'Server Error', error: error.message });
